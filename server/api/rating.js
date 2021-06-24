998ab3d51eca03d31b91a60576bdc1e9079c090e
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-24 17:17:25 -0400

    added post route for rating

---
+++ server/api/rating.js
@@ -0,0 +1,21 @@
+const router = require("express").Router();
+const {
+	models: { Rating },
+} = require("../db");
+const { requireToken } = require("./gatekeepingMiddleware");
+module.exports = router;
+
+//mounted on /api/rating
+
+//post routes
+router.post("/:snackId", requireToken, async (req, res, next) => {
+	const rating = await Rating.findOrCreate({
+		where: {
+			snackId: req.params.snackId,
+			userId: req.user.id,
+		},
+	});
+	await rating.update({ rating: req.body.rating });
+	await rating.save();
+	res.send(rating);
+});
