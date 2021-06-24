1d61e0718636811bc823c4a1eb3bb9e8606c89a4
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-24 17:39:42 -0400

    added api route to get a group's ratings

--- server/api/group.js
+++ server/api/group.js
@@ -8,15 +8,36 @@ module.exports = router;
 //mounted on /api/groups
 
 //get routes:
+router.get("/:groupId/ratings", requireToken, async (req, res, next) => {
+	try {
+		const group = (
+			await req.user.getGroups({
+				where: { groupId: req.params.groupId },
+				include: { model: User },
+			})
+		)[0];
+		if (!group.id)
+			res.status(403).send("You must be a member of a group to view it");
+		else {
+			const users = await group.getUsers({ include: { model: Rating } });
+			res.send(users);
+		}
+	} catch (err) {
+		next(err);
+	}
+});
+
 router.get("/:groupId", requireToken, async (req, res, next) => {
 	try {
-		const group = await req.user.getGroups({
-			where: { groupId: req.params.groupId },
-			include: { model: User },
-		});
-		if (!group.length)
+		const group = (
+			await req.user.getGroups({
+				where: { groupId: req.params.groupId },
+				include: { model: User },
+			})
+		)[0];
+		if (!group.id)
 			res.status(403).send("You must be a member of a group to view it");
-		else res.send(group[0]);
+		else res.send(group);
 	} catch (err) {
 		next(err);
 	}
