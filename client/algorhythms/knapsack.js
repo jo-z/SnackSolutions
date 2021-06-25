dffd5b0702bcd6b6c5291d731c476350712731d1
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-25 19:29:49 -0400

    added empty snacks arrays to inital matrix

--- client/algorhythms/knapsack.js
+++ client/algorhythms/knapsack.js
@@ -1,10 +1,10 @@
 export default (users, maxSnacks) => {
 	const peopleSatisfiedMatrix = [];
 	for (let i = 0; i <= users[0].snacks.length; i++) {
-		peopleSatisfiedMatrix.push([{ peopleSatisfied: 0 }]);
+		peopleSatisfiedMatrix.push([{ peopleSatisfied: 0, snacks: [] }]);
 	}
 	for (let i = 1; i <= maxSnacks; i++) {
-		peopleSatisfiedMatrix[0][i] = { peopleSatisfied: 0 };
+		peopleSatisfiedMatrix[0][i] = { peopleSatisfied: 0, snacks: [] };
 	}
 	console.log(peopleSatisfiedMatrix);
 };
