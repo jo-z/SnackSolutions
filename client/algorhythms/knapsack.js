65b789615feb34cd925f60319c6e59ec57888ea8
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-25 19:30:31 -0400

    chagned peopleSatisfied to arrays

--- client/algorhythms/knapsack.js
+++ client/algorhythms/knapsack.js
@@ -1,10 +1,10 @@
 export default (users, maxSnacks) => {
 	const peopleSatisfiedMatrix = [];
 	for (let i = 0; i <= users[0].snacks.length; i++) {
-		peopleSatisfiedMatrix.push([{ peopleSatisfied: 0, snacks: [] }]);
+		peopleSatisfiedMatrix.push([{ peopleSatisfied: [], snacks: [] }]);
 	}
 	for (let i = 1; i <= maxSnacks; i++) {
-		peopleSatisfiedMatrix[0][i] = { peopleSatisfied: 0, snacks: [] };
+		peopleSatisfiedMatrix[0][i] = { peopleSatisfied: [], snacks: [] };
 	}
 	console.log(peopleSatisfiedMatrix);
 };
