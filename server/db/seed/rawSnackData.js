dfa1be84d29966d35a1640d2a616b1abe1aff03a
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-25 11:05:40 -0400

    added seed data

---
+++ server/db/seed/rawSnackData.js
@@ -0,0 +1,24 @@
+const rawSnackData = [
+	{ name: "Sour Patch Kids" },
+	{ name: "Charcuterie" },
+	{ name: "Dried Mango" },
+	{ name: "French Fries" },
+	{ name: "Cheetos" },
+	{ name: "Plain Potato Chips" },
+	{ name: "Sour Cream and Onion Chips" },
+	{ name: "Barbecue Chips" },
+	{ name: `M&M's` },
+	{ name: "Orange Gummies" },
+	{ name: "Pizza" },
+	{ name: "Trail Mix" },
+	{ name: "Whoppers" },
+	{ name: "Oranges" },
+	{ name: "Strawberries" },
+	{ name: "Yakisoba" },
+	{ name: "Cheeze-its" },
+	{ name: "Cherries" },
+	{ name: "Swedish Fish" },
+	{ name: "Poki" },
+];
+
+module.exports = rawSnackData;
---
+++ server/db/seed/rawUserData.js
@@ -0,0 +1,91 @@
+const rawUserData = [
+	{
+		username: "spedroni0",
+		password: "Whpe2QKiO5pb",
+		email: "aridler0@usda.gov",
+	},
+	{ username: "agiraudeau1", password: "N9HDhNDT", email: "nizard1@go.com" },
+	{
+		username: "afermin2",
+		password: "3pkjMvJpl",
+		email: "swindridge2@salon.com",
+	},
+	{
+		username: "jyegoshin3",
+		password: "DxosYIg",
+		email: "tdarlow3@biblegateway.com",
+	},
+	{
+		username: "brankine4",
+		password: "8MwGWE",
+		email: "pstanistrete4@theglobeandmail.com",
+	},
+	{
+		username: "rchant5",
+		password: "UHetP5XQ",
+		email: "cklesel5@example.com",
+	},
+	{
+		username: "krapin6",
+		password: "uDnanbUUeei",
+		email: "wlempke6@wufoo.com",
+	},
+	{
+		username: "mboggs7",
+		password: "Sp6341DWUid",
+		email: "hmuriel7@mysql.com",
+	},
+	{
+		username: "mgallagher8",
+		password: "xZOGtz",
+		email: "dchitham8@noaa.gov",
+	},
+	{
+		username: "efero9",
+		password: "RzJgU97jBbc",
+		email: "cjayes9@bloglovin.com",
+	},
+	{
+		username: "cbagniuka",
+		password: "4TAMjuVVRL",
+		email: "bpelerina@slideshare.net",
+	},
+	{
+		username: "rtoothillb",
+		password: "e4pzWkFc2vC",
+		email: "ljoeb@chronoengine.com",
+	},
+	{
+		username: "sgaynec",
+		password: "eyOkSqRIo7",
+		email: "lchellamc@google.com.hk",
+	},
+	{ username: "iaustind", password: "1UCTPCUX", email: "ocaitlind@de.vu" },
+	{ username: "lablotte", password: "82q99s", email: "gmomerye@ihg.com" },
+	{
+		username: "ldalemanf",
+		password: "O7xI0GE",
+		email: "twincklesf@webmd.com",
+	},
+	{
+		username: "ematteuzzig",
+		password: "anN2IpjHZ",
+		email: "swellsg@issuu.com",
+	},
+	{
+		username: "amccorleyh",
+		password: "mrUYVcayws",
+		email: "rchateh@seesaa.net",
+	},
+	{
+		username: "kllelwelni",
+		password: "bifTqRUZX",
+		email: "smccookei@free.fr",
+	},
+	{
+		username: "jploughwrightj",
+		password: "NaFPfCt",
+		email: "jsherrinj@vinaora.com",
+	},
+];
+module.exports = rawUserData;
