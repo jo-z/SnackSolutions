d56f5bca3e5f581842f647b8dd25d75937724fc0
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-25 11:55:29 -0400

    disabled token requirements for testing

--- server/api/group.js
+++ server/api/group.js
@@ -2,43 +2,57 @@ const router = require("express").Router();
 const {
 	models: { Group, User, Rating },
 } = require("../db");
-const { requireToken } = require("./gatekeepingMiddleware");
+const {
+	/*requireToken*/
+} = require("./gatekeepingMiddleware");
 module.exports = router;
 
 //mounted on /api/groups
 
 //get routes:
-router.get("/:groupId/ratings", requireToken, async (req, res, next) => {
-	try {
-		const group = (
-			await req.user.getGroups({
-				where: { groupId: req.params.groupId },
-				include: { model: User },
-			})
-		)[0];
-		if (!group.id)
-			res.status(403).send("You must be a member of a group to view it");
-		else {
-			const users = await group.getUsers({ include: { model: Rating } });
-			res.send(users);
+router.get(
+	"/:groupId/ratings",
+	/*requireToken*/ async (req, res, next) => {
+		try {
+			const group = (
+				await req.user.getGroups({
+					where: { groupId: req.params.groupId },
+					include: { model: User },
+				})
+			)[0];
+			if (!group.id)
+				res.status(403).send(
+					"You must be a member of a group to view it"
+				);
+			else {
+				const users = await group.getUsers({
+					include: { model: Rating },
+				});
+				res.send(users);
+			}
+		} catch (err) {
+			next(err);
 		}
-	} catch (err) {
-		next(err);
 	}
-});
+);
 
-router.get("/:groupId", requireToken, async (req, res, next) => {
-	try {
-		const group = (
-			await req.user.getGroups({
-				where: { groupId: req.params.groupId },
-				include: { model: User },
-			})
-		)[0];
-		if (!group.id)
-			res.status(403).send("You must be a member of a group to view it");
-		else res.send(group);
-	} catch (err) {
-		next(err);
+router.get(
+	"/:groupId",
+	/*requireToken*/ async (req, res, next) => {
+		try {
+			const group = (
+				await req.user.getGroups({
+					where: { groupId: req.params.groupId },
+					include: { model: User },
+				})
+			)[0];
+			if (!group.id)
+				res.status(403).send(
+					"You must be a member of a group to view it"
+				);
+			else res.send(group);
+		} catch (err) {
+			next(err);
+		}
 	}
-});
+);
--- server/api/rating.js
+++ server/api/rating.js
@@ -2,24 +2,27 @@ const router = require("express").Router();
 const {
 	models: { Rating },
 } = require("../db");
-const { requireToken } = require("./gatekeepingMiddleware");
+// const { /*requireToken*/ } = require("./gatekeepingMiddleware");
 module.exports = router;
 
 //mounted on /api/rating
 
 //post routes
-router.post("/:snackId", requireToken, async (req, res, next) => {
-	try {
-		const rating = await Rating.findOrCreate({
-			where: {
-				snackId: req.params.snackId,
-				userId: req.user.id,
-			},
-		});
-		await rating.update({ rating: req.body.rating });
-		await rating.save();
-		res.send(rating);
-	} catch (err) {
-		next(err);
+router.post(
+	"/:snackId",
+	/*requireToken*/ async (req, res, next) => {
+		try {
+			const rating = await Rating.findOrCreate({
+				where: {
+					snackId: req.params.snackId,
+					userId: req.user.id,
+				},
+			});
+			await rating.update({ rating: req.body.rating });
+			await rating.save();
+			res.send(rating);
+		} catch (err) {
+			next(err);
+		}
 	}
-});
+);
