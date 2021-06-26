43445ae348e2050db7f3b706ac77a24d730d12fc
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-26 17:59:00 -0400

    hooked component up to store

--- client/components/Ratings.js
+++ client/components/Ratings.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { getUsersRatings } from "../store/ratings";
 
 export class Ratings extends React.Component {
 	render() {
@@ -7,4 +8,8 @@ export class Ratings extends React.Component {
 	}
 }
 
-export default connect(null, null)(Ratings);
+const mapState = (state) => ({ ratings: state.ratings });
+const mapDispatch = (dispatch) => ({
+	getRatings: () => dispatch(getUsersRatings()),
+});
+export default connect(mapState, mapDispatch)(Ratings);
