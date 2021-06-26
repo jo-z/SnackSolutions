b36e76566045d42e0fe1cdbd48fac9cc604a76c5
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-26 18:41:10 -0400

    added store functions to update ratings

--- client/store/ratings.js
+++ client/store/ratings.js
@@ -1,16 +1,18 @@
 import axios from "axios";
+const token = window.localStorage.getItem("token");
 
 //action types
 const SET_USERS_RATINGS = "SET_USERS_RATINGS";
+const UPDATE_RATING = "UPDATE_RATING";
 
 //action creators
 const setUsersRatings = (ratings) => ({ type: SET_USERS_RATINGS, ratings });
+const _updateRating = (rating) => ({ type: UPDATE_RATING, rating });
 
 //thunks
 export const getUsersRatings = () => {
 	return async (dispatch) => {
 		try {
-			const token = window.localStorage.getItem("token");
 			const ratings = (
 				await axios.get("/api/rating", {
 					headers: { authorization: token },
@@ -23,11 +25,30 @@ export const getUsersRatings = () => {
 	};
 };
 
+export const updateRating = (snackId, rating) => {
+	return async (dispatch) => {
+		try {
+			const updatedRating = (await axios.put(`/api/rating/${snackId}`),
+			{ rating },
+			{ headers: { authorization: token } }).data;
+			dispatch(_updateRating(updatedRating));
+		} catch (err) {
+			console.error(err);
+		}
+	};
+};
+
 //reducer
 export default function (state = [], action) {
 	switch (action.type) {
 		case SET_USERS_RATINGS:
 			return action.ratings;
+		case UPDATE_RATING:
+			return state.map((val) => {
+				if (val.id == action.updatedRating.snackId) {
+					return { ...val, rating: action.updateRating };
+				} else return val;
+			});
 		default:
 			return state;
 	}
