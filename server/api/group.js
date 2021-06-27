cdc6a1dfd1baaf7581032276c59207d24e831a49
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-27 13:45:13 -0400

    added put /api/group/groupId route

--- server/api/group.js
+++ server/api/group.js
@@ -68,23 +68,19 @@ router.get("/", requireToken, async (req, res, next) => {
 	}
 });
 
-// router.get("/:groupId", requireToken, async (req, res, next) => {
-// 	try {
-// 		// const user = await User.findByPk(req.body.id);
-// 		const group = await req.user.getGroups({
-// 			// (
-// 			// 	await user.getGroups({
-// 			where: { id: req.params.groupId },
-// 			// include: { model: User },
-// 		})[0];
-// 		if (!group.id)
-// 			res.status(403).send("You must be a member of a group to view it");
-// 		else res.send(group);
-// 		// const group = await Group.findByPk(req.params.groupId, {
-// 		// 	include: { model: User, attributes: ["id", "username"] },
-// 		// });
-// 		// res.send(group);
-// 	} catch (err) {
-// 		next(err);
-// 	}
-// });
+//put routes
+router.put(
+	"/:groupId",
+	requireToken,
+	checkMembership,
+	async (req, res, next) => {
+		try {
+			if (req.membership.isMember) {
+				await req.group.update({ name: req.body.name });
+				res.json(req.group);
+			} else res.status("403").send("You must be a member");
+		} catch (err) {
+			next(err);
+		}
+	}
+);
