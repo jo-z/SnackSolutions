39023229d680b3bbf50f13916f0311bc5cf93dab
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-27 11:24:31 -0400

    refactored get /:groupId/ratings to use checkMembership middleware

--- server/api/group.js
+++ server/api/group.js
@@ -1,56 +1,63 @@
 const router = require("express").Router();
 const {
-	models: { Group, User, Snack, Rating },
+	models: { Group, User, Snack },
 } = require("../db");
-const { requireToken } = require("./gatekeepingMiddleware");
+const { requireToken, checkMembership } = require("./gatekeepingMiddleware");
 module.exports = router;
 
 //mounted on /api/groups
 
 //get routes:
-router.get("/:groupId/ratings", requireToken, async (req, res, next) => {
-	try {
-		// const user = await User.findByPk(req.id);
-		const group = ( // await req.user.getGroups({
-			await req.user.getGroups({
-				where: { id: req.params.groupId },
-				// include: { model: User },
-			})
-		)[0];
-		if (!group.id)
-			res.status(403).send("You must be a member of a group to view it");
-		else {
-			const users = await User.findAll({
-				attributes: ["id", "username"],
-				include: [
-					{
-						model: Group,
-						where: { id: req.params.groupId },
-						attributes: [],
-					},
-					{
-						model: Snack,
-						attributes: [
-							"id",
-							"name",
-							"isVegan",
-							"isKosher",
-							"isHalal",
-							"isDairyFree",
-							"isGlutenFree",
-							"isNutFree",
-							"isVegetarian",
-						],
-						through: { attributes: ["rating"] },
-					},
-				],
-			});
-			res.send(users);
+router.get(
+	"/:groupId/ratings",
+	requireToken,
+	checkMembership,
+	async (req, res, next) => {
+		try {
+			// const user = await User.findByPk(req.id);
+			// const group = ( // await req.user.getGroups({
+			// 	await req.user.getGroups({
+			// 		where: { id: req.params.groupId },
+			// 		// include: { model: User },
+			// 	})
+			// )[0];
+			if (!req.membership.isMember)
+				res.status(403).send(
+					"You must be a member of a group to view it"
+				);
+			else {
+				const users = await User.findAll({
+					attributes: ["id", "username"],
+					include: [
+						{
+							model: Group,
+							where: { id: req.params.groupId },
+							attributes: [],
+						},
+						{
+							model: Snack,
+							attributes: [
+								"id",
+								"name",
+								"isVegan",
+								"isKosher",
+								"isHalal",
+								"isDairyFree",
+								"isGlutenFree",
+								"isNutFree",
+								"isVegetarian",
+							],
+							through: { attributes: ["rating"] },
+						},
+					],
+				});
+				res.send(users);
+			}
+		} catch (err) {
+			next(err);
 		}
-	} catch (err) {
-		next(err);
 	}
-});
+);
 
 router.get("/", requireToken, async (req, res, next) => {
 	try {
