cc411f9aaf8fddaf85ebb99c5fa8a1c77bf5d443
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-26 20:05:59 -0400

    added get route to get list of user's groups

--- server/api/group.js
+++ server/api/group.js
@@ -52,28 +52,32 @@ router.get("/:groupId/ratings", requireToken, async (req, res, next) => {
 	}
 });
 
-router.get(
-	"/:groupId",
-	/*requireToken*/ async (req, res, next) => {
-		try {
-			// const user = await User.findByPk(req.body.id);
-			// const group = ( // await req.user.getGroups({
-			// 	await user.getGroups({
-			// 		where: { id: req.params.groupId },
-			// 		// include: { model: User },
-			// 	})
-			// )[0];
-			// if (!group.id)
-			// 	res.status(403).send(
-			// 		"You must be a member of a group to view it"
-			// 	);
-			// else res.send(group);
-			const group = await Group.findByPk(req.params.groupId, {
-				include: { model: User, attributes: ["id", "username"] },
-			});
-			res.send(group);
-		} catch (err) {
-			next(err);
-		}
+router.get("/", requireToken, async (req, res, next) => {
+	try {
+		const groups = await req.user.getGroups();
+		res.json(groups);
+	} catch (err) {
+		next(err);
 	}
-);
+});
+
+// router.get("/:groupId", requireToken, async (req, res, next) => {
+// 	try {
+// 		// const user = await User.findByPk(req.body.id);
+// 		const group = await req.user.getGroups({
+// 			// (
+// 			// 	await user.getGroups({
+// 			where: { id: req.params.groupId },
+// 			// include: { model: User },
+// 		})[0];
+// 		if (!group.id)
+// 			res.status(403).send("You must be a member of a group to view it");
+// 		else res.send(group);
+// 		// const group = await Group.findByPk(req.params.groupId, {
+// 		// 	include: { model: User, attributes: ["id", "username"] },
+// 		// });
+// 		// res.send(group);
+// 	} catch (err) {
+// 		next(err);
+// 	}
+// });
