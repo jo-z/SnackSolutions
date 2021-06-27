44b8988b25495546dc15a938aca5b87b478e3344
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-26 23:18:01 -0400

    wrote basic skeleton of RateSnacks component

---
+++ client/components/RateSnacks.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class RateSnacks extends React.component {
+	render() {
+		return <p>hello!!!</p>;
+	}
+}
+export default connect()(RateSnacks);
