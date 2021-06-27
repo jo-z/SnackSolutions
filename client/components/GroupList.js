c55b6b86e038840f34618d4b8649559fa81208f2
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-26 20:28:18 -0400

    GroupList now lists user's groups

--- client/components/GroupList.js
+++ client/components/GroupList.js
@@ -1,10 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
 import { getGroupList } from "../store/groups";
+import Link from "react-router-dom/Link";
 
 class GroupList extends React.Component {
+	componentDidMount() {
+		this.props.getGroups();
+	}
 	render() {
-		return <p>hello!!</p>;
+		const groups = this.props.groups || [];
+		return (
+			<div>
+				{groups.map((group) => (
+					<Link to={`/groups/${group.id}`} key={group.id}>
+						{group.name}
+					</Link>
+				))}
+			</div>
+		);
 	}
 }
 
