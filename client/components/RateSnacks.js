eb59572a7730c9daeb0772354b318cc6d502ba9d
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-26 23:40:43 -0400

    added fallback definiton of snacks

--- client/components/RateSnacks.js
+++ client/components/RateSnacks.js
@@ -3,12 +3,13 @@ import { connect } from "react-redux";
 import { getUnratedSnacks } from "../store/unratedSnacks";
 import RatingPanel from "./RatingPanel";
 
-class RateSnacks extends React.component {
+class RateSnacks extends React.Component {
 	componentDidMount() {
 		this.props.getUnratedSnacks();
 	}
 	render() {
-		return this.props.snacks.map((val) => {
+		const snacks = this.props.snacks || [];
+		return snacks.map((val) => {
 			return (
 				<RatingPanel
 					key={val.id}
