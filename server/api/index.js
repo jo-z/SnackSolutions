e20bb7e6d8322e74c4e44e9e46ec7137d4a6adb0
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-27 11:01:23 -0400

    hooked up new api router

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,7 @@ router.use("/users", require("./users"));
 router.use("/snacks", require("./snacks"));
 router.use("/rating", require("./rating"));
 router.use("/group", require("./group"));
+router.use("/membership", require("./membership"));
 
 router.use((req, res, next) => {
 	const error = new Error("Not Found");
