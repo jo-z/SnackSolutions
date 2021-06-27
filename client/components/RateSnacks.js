70170e8be29dbcb5661f64750411bb70ec645f61
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-26 23:35:40 -0400

    filled in RateSnacks component

--- client/components/RateSnacks.js
+++ client/components/RateSnacks.js
@@ -1,9 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
+import { getUnratedSnacks } from "../store/unratedSnacks";
+import RatingPanel from "./RatingPanel";
 
 class RateSnacks extends React.component {
+	componentDidMount() {
+		this.props.getUnratedSnacks();
+	}
 	render() {
-		return <p>hello!!!</p>;
+		return this.props.snacks.map((val) => {
+			return (
+				<RatingPanel
+					key={val.id}
+					snack={{ ...val, rating: { rating: null } }}
+				/>
+			);
+		});
 	}
 }
-export default connect()(RateSnacks);
+
+const mapState = (state) => ({ snacks: state.unratedSnacks });
+const mapDispatch = (dispatch) => ({
+	getUnratedSnacks: () => dispatch(getUnratedSnacks()),
+});
+export default connect(mapState, mapDispatch)(RateSnacks);
