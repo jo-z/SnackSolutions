187349d900722c474eff1c2e632ae439113d47f8
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-27 14:26:22 -0400

    wrote create group slice of store

--- client/store/groups.js
+++ client/store/groups.js
@@ -7,6 +7,7 @@ const authorization = {
 //action types
 const SET_GROUP_LIST = "SET_GROUP_LIST";
 export const UPDATE_GROUP = "UPDATE GROUP";
+const CREATE_GROUP = "CREATE_GROUP";
 
 //action creators
 const setGroupList = (groups) => ({
@@ -15,6 +16,10 @@ const setGroupList = (groups) => ({
 });
 const _updateGroup = (group) => ({
 	type: UPDATE_GROUP,
+	group,
+});
+const _createGroup = (group) => ({
+	type: CREATE_GROUP,
 	group,
 });
 
@@ -45,12 +50,26 @@ export const updateGroup = (groupId, group) => {
 		}
 	};
 };
+export const createGroup = (group) => {
+	return async (dispatch) => {
+		try {
+			const newGroup = (
+				await axios.post("/api/group", group, authorization)
+			).data;
+			dispatch(_createGroup(newGroup));
+		} catch (err) {
+			console.error(err);
+		}
+	};
+};
 
 //reducer
 export default function (state = [], action) {
 	switch (action.type) {
 		case SET_GROUP_LIST:
 			return action.groups;
+		case CREATE_GROUP:
+			return [...state, action.group];
 		default:
 			return state;
 	}
