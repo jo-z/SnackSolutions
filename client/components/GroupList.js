7e9a4b906f2bb730b2d02664141efe2cc8cc044f
Josephine Bartholoma <josephine.bartholoma@gmail.com>
2021-06-26 20:14:15 -0400

    mapped redux components to props

--- client/components/GroupList.js
+++ client/components/GroupList.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
+import { getGroupList } from "../store/groups";
 
 class GroupList extends React.Component {
 	render() {
 		return <p>hello!!</p>;
 	}
 }
-export default connect()(GroupList);
+
+const mapState = (state) => ({ groups: state.groups });
+const mapDispatch = (dispatch) => ({
+	getGroups: () => dispatch(getGroupList()),
+});
+
+export default connect(mapState, mapDispatch)(GroupList);
